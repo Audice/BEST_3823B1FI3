96790d22ab8223c1c9aeaf9f7666c64ca3d7b07a
Audice <karchkov.denis@mail.ru>
2023-09-23 16:08:07 +0300

    New try

--- 1-type/Task2/test.c
+++ 1-type/Task2/test.c
@@ -6,7 +6,7 @@ void test_simple() {
     TEST_ASSERT(sum(1, 3) == 3);
     TEST_ASSERT(sum(6, 7) == 42);
     TEST_ASSERT(sum(-5, 5) == -25);
-    TEST_ASSERT(sum(34, 0) == 34);
+    TEST_ASSERT(sum(34, 0) == 0);
 }
 
 TEST_LIST= {
