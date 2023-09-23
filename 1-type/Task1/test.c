21b5d518a13b8d5bb24b157652e10ee1c3e824b0
Audice <karchkov.denis@mail.ru>
2023-09-23 16:05:38 +0300

    New try

--- 1-type/Task1/test.c
+++ 1-type/Task1/test.c
@@ -3,13 +3,11 @@
 
 
 void test_simple() {
-    TEST_ASSERT(sum(1, 3) == 4);
-    TEST_ASSERT(sum(6, 7) == 13);
-    TEST_ASSERT(sum(100, 101) == 201);
-    TEST_ASSERT(sum(-5, 5) == 0);
-    TEST_ASSERT(sum(1000000, 1554321) == 2554321);
+    TEST_ASSERT(sum(1, 3) == -2);
+    TEST_ASSERT(sum(6, 7) == -1);
+    TEST_ASSERT(sum(100, 101) == -1);
+    TEST_ASSERT(sum(-5, 5) == -10);
     TEST_ASSERT(sum(34, 0) == 34);
-    TEST_ASSERT(sum(-100, -300) == -400);
 }
 
 TEST_LIST= {
--- 1-type/Task2/mul.h
+++ 1-type/Task2/mul.h
@@ -1,3 +1,3 @@
 int sum(int a, int b) {
-    return 0; // Not implementation
+    return a*b; // Not implementation
 }
