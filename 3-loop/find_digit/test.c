0cc401cc90597ae393ff54e7255178df65111361
Karchkov Denis <karchkov.denis@mail.ru>
2023-10-27 12:39:11 +0300

    Correct

--- 3-loop/find_digit/test.c
+++ 3-loop/find_digit/test.c
@@ -5,7 +5,7 @@
 void test_find_digit() {
     TEST_ASSERT(find_digit(8800.555353518, 3) == 9);
     TEST_ASSERT(find_digit(1111.101010993, 10) == -1);
-    TEST_ASSERT(find_digit(1.23457880, 3) == -1);
+    TEST_ASSERT(find_digit(1.23457880, 3) == 4);
     TEST_ASSERT(find_digit(9.99111, 1) == 5);
     TEST_ASSERT(find_digit(18335, -1) == -1);
     TEST_ASSERT(find_digit(11335593925544, 2) == 10);
--- 3-loop/integral/test.c
+++ 3-loop/integral/test.c
@@ -6,7 +6,7 @@ void test_integral() {
     TEST_ASSERT(fabs(integral(1, 1, 0, -2, 1) - 1.5) < 0.01);
     TEST_ASSERT(fabs(integral(1, 1, 1, -1, 1) - 2.66666) < 0.01);
     TEST_ASSERT(fabs(integral(-1, 1, 0, 0, 1) - 0.166666) < 0.00001);
-    TEST_ASSERT(fabs(integral(1, 1, 0, -1, 0) - 1) < 0.00001);
+    TEST_ASSERT(fabs(integral(1, 1, 0, -1, 0)) - 1 < 0.00001);
 }
 
 TEST_LIST = {
