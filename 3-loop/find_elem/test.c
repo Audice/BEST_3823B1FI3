9e94655a557d9eaa9085fab37f56d40c6837bf8c
Karchkov Denis <karchkov.denis@mail.ru>
2023-11-03 19:47:47 +0300

    Correcting

--- 3-loop/find_elem/test.c
+++ 3-loop/find_elem/test.c
@@ -7,7 +7,7 @@ void test_find_number() {
     TEST_ASSERT(find_elem(0, 100, 5, 5) == 20);
     TEST_ASSERT(find_elem(0, 1000, 500, 1) == 500);
     TEST_ASSERT(find_elem(0, 1000, 333, 11) == 3330);
-    TEST_ASSERT(find_elem(0, 10000, 9999, 13) == 119988);
+    TEST_ASSERT(find_elem(0, 10000, 9999, 15) == 129987);
 }
 
 TEST_LIST = {
