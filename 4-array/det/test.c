3f6ced5bf847372d57b3f9c35c6c07eebd892737
Karchkov Denis <karchkov.denis@mail.ru>
2023-11-08 14:09:06 +0300

    ...

--- 4-array/det/test.c
+++ 4-array/det/test.c
@@ -8,7 +8,7 @@ void test_determinant() {
     int matrix3[4] = {1, 0, 0, 1};
     int matrix4[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int matrix5[16] = {1, 1, 1, 1, 0, 2, 6, 7, 9, 8, 7, 5, -5, 7, 3, 2};
-    int matrix6[25] = {1, 0, 0, 0, 0, 5, 5, 5, 5, 5, -1, -1, -1, -1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1};
+    int matrix6[25] = {1, 0, 0, 0, 0, 5, 5, 5, 5, 5, -1, -1, -2, -1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1};
     
     TEST_ASSERT(determinant(matrix1, sizeof(matrix1)/sizeof(int)) == 1);
     TEST_ASSERT(determinant(matrix2, sizeof(matrix2)/sizeof(int)) == -404);
