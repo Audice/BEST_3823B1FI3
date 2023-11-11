d769ddc76e41a0f1c5be9680912cba7bc3470983
Karchkov Denis <karchkov.denis@mail.ru>
2023-11-11 16:01:00 +0300

    Correct

--- 4-array/little_big_converter/test.c
+++ 4-array/little_big_converter/test.c
@@ -3,11 +3,11 @@
 
 
 void test_converter() {
-    TEST_ASSERT(little_big_convert(3) == 3);
-    TEST_ASSERT(little_big_convert(123456) == 4252161);
+    TEST_ASSERT(little_big_convert(3) == 50331648);
+    TEST_ASSERT(little_big_convert(123456) == 1088553216);
     TEST_ASSERT(little_big_convert(100000000) == 14808325);
-    TEST_ASSERT(little_big_convert(256) == 1);
-    TEST_ASSERT(little_big_convert(255) == 255);
+    TEST_ASSERT(little_big_convert(256) == 65536);
+    TEST_ASSERT(little_big_convert(255) == 4278190080);
     TEST_ASSERT(little_big_convert(4294967295) == 4294967295);
 }
 
